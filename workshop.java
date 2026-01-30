0d945e14299bfd51a7c842e820078b185907f333
choudharydhruv590-prog <choudharydhruv590@gmail.com>
2026-01-30 09:56:23 +0530

    Add files via upload

---
+++ workshop.java
@@ -0,0 +1,14 @@
+
+ class Name
+ {
+	 public static void main(String[]args){
+    String[]names=new String[4];
+	names[0]="Harshit";
+	names[1]="Dhruv";
+	names[2]="Sagar";
+	names[3]="varun";
+	for(int i=0;i<=3;i++)
+		system.out.println(names[i]);
+	 }
+ }
+ }
